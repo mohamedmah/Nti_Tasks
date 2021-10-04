bdad57f7b4a773f6b9a2edf5ba11d663027cc12f
Mohamed Maher <50726198+mohamedmah@users.noreply.github.com>
2021-10-04 06:08:40 +0200

    Add files via upload

---
+++ calculator.php
@@ -0,0 +1,24 @@
+<?php
+$num = 10;
+
+switch ($num) {
+  case 10:
+    echo $num+2;
+    echo "<br>";
+ 
+  case 10:
+    echo $num-2;
+    echo "<br>";
+    
+  case 10:
+    echo $num*2;
+    echo "<br>";
+   
+  case 10:
+        echo $num/2;
+        echo "<br>";
+       
+}
+
+
+?>
